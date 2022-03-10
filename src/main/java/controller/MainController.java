80e4a32c0475265676face7574251d960dfd0b76
limhalim <gkfla668@naver.com>
2022-03-11 02:15:22 +0900

    feat: 입력 받은 사용자와 딜러를 포함한 플레이어 구하는 메서드 추가

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -10,12 +10,26 @@ import java.util.List;
 
 public class MainController {
     public void play() {
-        String names = InputView.inputPlayerNames(); // view 호출, 게임의 참여할 사람 이름 입력 받기
-        String[] users = Player.splitPlayerNames(names); // model 호출, 쉼표 기준으로 잘라서 배열에 저장
+        String[] users = getUsers();
+        List<String> players = getPlayers(users);
+        createPlayer(players);
+    }
 
+    public List<String> getPlayers(String[] users) {
         List<String> players = new ArrayList<>(Arrays.asList(users)); // 배열 리스트로
         players.add(0, "딜러"); // 딜러 추가
 
+        return players;
+    }
+
+    public String[] getUsers() {
+        String names = InputView.inputPlayerNames(); // view 호출, 게임의 참여할 사람 이름 입력 받기
+        String[] users = Player.splitPlayerNames(names); // model 호출, 쉼표 기준으로 잘라서 배열에 저장
+
+        return users;
+    }
+
+    public void createPlayer(List<String> players) {
         OutputView.printPlayerInitialMessage(players); // 안내 메세지 출력
         for (String name : players) {
             Player player = createPlayerCard(name);
