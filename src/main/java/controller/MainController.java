cba6808d47ba8a19cdd733d5929ccdd5cc98fe05
limhalim <gkfla668@naver.com>
2022-03-11 02:10:23 +0900

    refactor: 한명의 플레이어의 카드를 생성하는 코드 메서드로 분리

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -8,20 +8,26 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-
 public class MainController {
     public void play() {
         String names = InputView.inputPlayerNames(); // view 호출, 게임의 참여할 사람 이름 입력 받기
         String[] users = Player.splitPlayerNames(names); // model 호출, 쉼표 기준으로 잘라서 배열에 저장
-        List<String> players = new ArrayList<>(Arrays.asList(users));
-        players.add(0, "딜러");
 
-        OutputView.printPlayerInitialMessage(players); // 메세지 출력
+        List<String> players = new ArrayList<>(Arrays.asList(users)); // 배열 리스트로
+        players.add(0, "딜러"); // 딜러 추가
+
+        OutputView.printPlayerInitialMessage(players); // 안내 메세지 출력
         for (String name : players) {
-            List<String> cards = Player.getCard(); // player의 2장의 카드를 담은 list
-            Player player = new Player(name, cards);
-            OutputView.printPlayerOwnCard(player);
+            Player player = createPlayerCard(name);
+            OutputView.printPlayerOwnCard(player); // 생성된 카드 출력
         }
+    }
+
+    public Player createPlayerCard(String name) {
+        List<String> cards = Player.getCard(); // player의 2장의 카드를 담은 list
+        Player player = new Player(name, cards);
+
+        return player;
     }
 
 
