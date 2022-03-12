07413309e6b1ba0eae329586337da448be0de7a1
Egenieee <hhgg0925@naver.com>
2022-03-12 20:53:47 +0900

    feat: Card class 추가

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Card {
+    private String card;
+
+    public Card(String cardType) {
+        this.card = cardType;
+    }
+
+    public String getCard() {
+        return card;
+    }
+}
