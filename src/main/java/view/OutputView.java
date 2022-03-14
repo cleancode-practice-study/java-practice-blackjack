c3482d7ea2ea4b05365f4f9b8a00636e27a6e7ce
Egenieee <hhgg0925@naver.com>
2022-03-14 09:48:32 +0900

    -refactor: OutputView 주석 삭제

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,10 +1,5 @@
 package view;
 
-//딜러와 halim, jinhee에게 2장의 카드를 나누었습니다.
-//        딜러 카드: 3다이아몬드, 9클로버
-//        halim 카드: 2하트, 8스페이드
-//        jinhee 카드: 7클로버, K스페이드
-
 public class OutputView {
     private static final String INIT_CARD_SETTING_OUTPUT_MESSAGE = "%s와 %s에게 2장의 카드를 나누었습니다.\n";
     private static final String ADDITIONAL_CARD_OUTPUT_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.\n";
