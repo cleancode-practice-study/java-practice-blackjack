945b077f736f0d5dd4b2b2ec94b8b0b251fb3676
Egenieee <hhgg0925@naver.com>
2022-03-31 07:29:42 +0900

    -refactor: Card class 추가

---
+++ src/main/java/model/Card.java
@@ -0,0 +1,16 @@
+package model;
+
+// 카드가 가지고 있을 만한 정보?
+// 카드 이름
+public class Card {
+    String card;
+
+    public Card(String card) {
+        this.card = card;
+    }
+
+    public String getCard() {
+        return card;
+    }
+
+}
