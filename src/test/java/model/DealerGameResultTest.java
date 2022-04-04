60c04108ade51664a694a3925698230c957cae3f
Egenieee <hhgg0925@naver.com>
2022-04-04 10:53:32 +0900

    -test: DealerGameResultTest 작성

---
+++ src/test/java/model/DealerGameResultTest.java
@@ -0,0 +1,24 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DealerGameResultTest {
+    @Test
+    public void 딜러_게임_결과_확인() {
+        Map<String, Integer> gameResult = new HashMap<>();
+
+        gameResult.put(GameResultType.WIN.getCardType(),
+                gameResult.getOrDefault(GameResultType.WIN.getCardType(), 0) + 1);
+
+        gameResult.put(GameResultType.WIN.getCardType(),
+                gameResult.getOrDefault(GameResultType.WIN.getCardType(), 0) + 1);
+
+        DealerGameResult dealerGameResult = new DealerGameResult(gameResult);
+
+        assertThat(dealerGameResult.getResultForPrint()).contains("2승");
+    }
+}
