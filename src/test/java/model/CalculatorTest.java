46abea3d53c1d44417f3c3436aabca3e3d115592
Egenieee <hhgg0925@naver.com>
2022-04-04 10:00:49 +0900

    -test: CalculatorTest 작성

---
+++ src/test/java/model/CalculatorTest.java
@@ -0,0 +1,37 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CalculatorTest {
+    @Test
+    public void A_없는_카드_합_계산() {
+        Cards cards = new Cards(Arrays.asList(new Card("3하트"), new Card("J다이아몬드"), new Card("7스페이드")));
+
+        int cardSum = Calculator.getCardSum(cards);
+
+        assertThat(cardSum).isEqualTo(20);
+    }
+
+    @Test
+    public void A_11로_계산하는_경우() {
+        Cards cards = new Cards(Arrays.asList(new Card("A하트"), new Card("J다이아몬드")));
+
+        int cardSum = Calculator.getCardSum(cards);
+
+        assertThat(cardSum).isEqualTo(21);
+    }
+
+    @Test
+    public void A_1로_계산하는_경우() {
+        Cards cards = new Cards(Arrays.asList(new Card("A하트"), new Card("J다이아몬드"), new Card("K클로버")));
+
+        int cardSum = Calculator.getCardSum(cards);
+
+        assertThat(cardSum).isEqualTo(21);
+    }
+
+}
