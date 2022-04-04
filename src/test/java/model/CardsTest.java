79f8cf4cbe190fcb036629d120fc959c71b1fc1e
Egenieee <hhgg0925@naver.com>
2022-04-04 10:17:30 +0900

    -test: CardsTest 작성

---
+++ src/test/java/model/CardsTest.java
@@ -0,0 +1,19 @@
+package model;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CardsTest {
+    @Test
+    public void 카드_생성() {
+        Cards cards = new Cards(Arrays.asList(new Card("3하트"), new Card("J다이아몬드"), new Card("7스페이드")));
+
+        assertThat(cards.getCards().size()).isEqualTo(3);
+        assertThat(cards.getCards().get(0).getCard()).isEqualTo("3하트");
+        assertThat(cards.getCards().get(1).getCard()).isEqualTo("J다이아몬드");
+        assertThat(cards.getCards().get(2).getCard()).isEqualTo("7스페이드");
+    }
+}
