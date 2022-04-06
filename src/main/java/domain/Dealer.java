165cbe15e6f295b7a305102e2fcf9bd9c1756a33
Egenieee <hhgg0925@naver.com>
2022-04-07 08:14:26 +0900

    -refactor: Dealer 객체 상수 이용하도록 수정

--- src/main/java/domain/Dealer.java
+++ src/main/java/domain/Dealer.java
@@ -1,13 +1,15 @@
 package domain;
 
 public class Dealer extends Player {
+    private static final int ADDITIONAL_CARD_STANDARD = 16;
+
     public Dealer(Cards cards) {
         this.name = "딜러";
         this.cards = cards;
     }
 
     public boolean isEnough() {
-        return Calculator.getCardSum(cards) <= 16;
+        return Calculator.getCardSum(cards) <= ADDITIONAL_CARD_STANDARD;
     }
 
     public String getGameResult(String result) {
